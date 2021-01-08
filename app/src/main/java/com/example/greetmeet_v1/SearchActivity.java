4551b9fff067d5149884fd1087cb2479d0ba643c
Jukocross <jukocross@gmail.com>
2021-01-08 14:54:57 +0800

    Updated Search Features

--- app/src/main/java/com/example/greetmeet_v1/SearchActivity.java
+++ app/src/main/java/com/example/greetmeet_v1/SearchActivity.java
@@ -2,20 +2,59 @@ package com.example.greetmeet_v1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private SearchAdapter adapter;
+    private RecyclerView recyclerView;
+    private DatabaseReference reff;
+    private ArrayList<Group> list;
+    SearchAdapter.RecyclerViewClickListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        reff = FirebaseDatabase.getInstance().getReference().child("Group");
+
+        list = new ArrayList<Group>();
+
+        recyclerView = findViewById(R.id.searchRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                //Toast.makeText(MainFeed.this,list.get(position).getGroupName(),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SearchActivity.this,EventDetails.class);
+                intent.putExtra("groupId",list.get(position).getGroupId());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+            }
+        }));
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
@@ -46,5 +85,48 @@ public class SearchActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        SearchView searchView = (SearchView) findViewById(R.id.searchView);
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+    }
+
+    protected void onStart() {
+        if (list != null){
+            list.clear();
+        }
+        super.onStart();
+        if (reff != null) {
+            reff.addValueEventListener(new ValueEventListener() {
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    //check for deleted item
+
+                    if (dataSnapshot.exists()) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            if (!ds.getValue(Group.class).getDeleted()) {
+                                list.add(0, ds.getValue(Group.class));
+                            }
+                        }
+                        adapter = new SearchAdapter(list, listener);
+                        recyclerView.setAdapter(adapter);
+                    }
+
+                }
+
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(SearchActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
